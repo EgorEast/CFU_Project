b57690369f9da0d30f07d9a69a58b94b29eb9c39
EgorEast <jir.jir35@gmail.com>
2020-08-20 13:16:39 +0300

    Deleted unnecessary

--- Cli-Serv TD Slobodskoy/TD Slobodskoy Client - не работает/drawMap.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <SFML\Graphics.hpp>
-
-using namespace sf;
-//Отрисовка карты
-void drawMap(Sprite& spriteMap, RenderWindow& window, sf::String TileMap[], const int HEIGHT_MAP, const int WIDTH_MAP);
--- Cli-Serv TD Slobodskoy/TD Slobodskoy Server - не работает/levels.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#include "Levels/Level_1.h"
